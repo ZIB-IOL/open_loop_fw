6a197ab34f041a1d21abf8b298ba1a66eab05359
elwirth <elias.wirth@bluewin.ch>
2022-08-09 15:49:04 +0200

    larger plots

--- global_.py
+++ global_.py
@@ -6,9 +6,9 @@ COLORS = [ 'red', 'blue', 'orange',  'green', 'purple', 'magenta', 'cyan', 'oliv
 MARKERS = ['o', 's', 'v', 'd', 'x', 'o', 's', 'v', 'd', 'x', 'o', 's', 'v', 'd', 'x', 'o', 's', 'v', 'd', 'x',
            's', 'v', 'd', 'x', 'o', 's', 'v', 'd', 'x', 'o', 's', 'v', 'd', 'x', 'o', 's', 'v', 'd', 'x', 'o',
            'v', 'd', 'x', 'o', 's', 'v', 'd', 'x', 'o', 's', 'v', 'd', 'x', 'o', 's', 'v', 'd', 'x', 'o', 's']
-LINEWIDTH = 4
-FONT_SIZE = 24
-FONT_SIZE_LEGEND = 12
+LINEWIDTH = 5
+FONT_SIZE = 36
+FONT_SIZE_LEGEND = 28
 CHUNKSIZE = 10000
 
 
